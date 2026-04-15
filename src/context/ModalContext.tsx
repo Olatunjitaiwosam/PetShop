8333bf032b87ab0c8a9956bfde628493c1c59195
Olatunjitaiwosam <u2348125@uel.ac.uk>
2026-04-15 20:19:44 +0100

    fixing deloying bug

--- src/context/ModalContext.tsx
+++ src/context/ModalContext.tsx
@@ -1,25 +1,25 @@
-import React, {createContext, useState} from 'react'
+import React, { createContext, useState } from 'react'
 
-interface ModalContext{
-    modal: boolean
-    open: () => void
-    close: () => void
+interface ModalContextType {
+  modal: boolean
+  open: () => void
+  close: () => void
 }
 
-export const ModalContext = createContext({
-    modal: false,
-    open: () => {},
-    close: () => {}
+export const ModalContext = createContext<ModalContextType>({
+  modal: false,
+  open: () => {},
+  close: () => {}
 })
 
-export const ModalState = ({children}: {children: React.ReactNode}) => {
-    const [modal, setModal] = useState(false)
-    const open = () => setModal(true)
-    const close = () => setModal(false)
+export const ModalState = ({ children }: { children: React.ReactNode }) => {
+  const [modal, setModal] = useState(false)
+  const open = () => setModal(true)
+  const close = () => setModal(false)
 
-    return (
-        <ModalContext.Provider value={{modal, open, close}}>
-            {children}
-        </ModalContext.Provider>
-    )
+  return (
+    <ModalContext.Provider value={{ modal, open, close }}>
+      {children}
+    </ModalContext.Provider>
+  )
 }
